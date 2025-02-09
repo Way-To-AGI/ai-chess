1ff08ab38981cd0bb2742093fc1525a9be4dd64e
Mrdapeng <18508510+Mrdapeng@users.noreply.github.com>
2025-02-09 13:47:18 +0800

    feat: style

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  optimizeFonts: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
